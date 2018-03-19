d87d519da131ce5feb349eb0f902d2b1deb2c871
Muhammed K <mka281@icloud.com>
2018-03-19 10:43:01 +0000

    Define the Create route

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require("express"),
     app = express(),
-    port = process.env.PORT || 3000;
-  
+    port = process.env.PORT || 3000,
+    bodyParser = require("body-parser");
+ 
 var todoRoutes = require("./routes/todos");
-  
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get('/', function(req, res) {
     res.send("Hello from the root route");
 });
