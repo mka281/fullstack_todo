c3d02424b238534e63ff16e566f4d9cfb6c3180c
Muhammed K <mka281@icloud.com>
2018-03-19 18:29:25 +0000

    Serve static files

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static("public"));
+app.use(express.static(__dirname + "/views"));
 
 app.get('/', function(req, res) {
-    res.send("Hello from the root route");
+    res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
