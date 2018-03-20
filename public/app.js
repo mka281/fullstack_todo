1dfab6215fc773f099bf98f2b9e08ae53d88770c
Muhammed K <mka281@icloud.com>
2018-03-20 08:57:45 +0000

    Add Toggle completion functionality to list elements

--- public/app.js
+++ public/app.js
@@ -9,7 +9,12 @@ $(document).ready(function(){
     }
   });
   
-  $('.list').on('click', 'span', function(){
+  $('.list').on('click', 'li', function(){
+    updateTodo($(this));
+  });
+  
+  $('.list').on('click', 'span', function(event){
+    event.stopPropagation();
     removeTodo($(this).parent());
   });
 });
@@ -24,6 +29,7 @@ function addTodos(todos) {
 function addTodo(todo){
   var newTodo = $('<li class="task">'+todo.name +' <span>X</span></li>');
   newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
   if(todo.completed){
     newTodo.addClass("done");
   }
@@ -55,5 +61,20 @@ function removeTodo(todo){
   })
   .catch(function(err){
     console.log(err);
+  });
+}
+
+function updateTodo(todo){
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = {completed: isDone};
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo){
+    todo.toggleClass("done");
+    todo.data('completed', isDone);
   });
 }
